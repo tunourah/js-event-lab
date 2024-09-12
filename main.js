b2ad8c03dfeb3ea81ece5adc26fdf4419044b7c6
noura <noura.tu19@gmail.com>
2024-09-12 11:32:41 +0300

    color font

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let text =document.getElementById("text") 
 let img = document.getElementById('Image');
-
+let buttonimage =document.getElementById("buttonsix")
 document.getElementById("buttonone").addEventListener("click", function(){
  
     text.textContent = "Hello World!";
@@ -20,20 +20,18 @@ document.getElementById("buttonone").addEventListener("click", function(){
       
      document.getElementById("buttonfour").addEventListener("click", function(){
         text.style.fontSize = "2rem";
-        text.style.color = "skyblue";
+        text.style.color = "blue";
      });
      document.getElementById("buttonfive").addEventListener("click", function(){
         text.style.fontSize = "2rem";
         text.style.backgroundColor = "skyblue";
      });
-     document.getElementById("buttonsix").addEventListener("click", function(){   
-img.style.display = "block";
+     buttonimage.addEventListener("click", function() {
+        img.style.display = "block";
         img.src = "https://images.pexels.com/photos/20821498/pexels-photo-20821498/free-photo-of-a-cake-on-a-table.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load";
-
-     }  );
+    });
     
-     document.getElementById("buttonsix").addEventListener("click", function() {
-         
+    buttonimage.addEventListener("dblclick", function() {
         img.style.display = "block";
         img.src = "https://images.pexels.com/photos/8462116/pexels-photo-8462116.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load";
     });
